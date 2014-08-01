fc8e1838eecdcbb70de0925a044377ea472b1272
Brian Woodward <brian.woodward@gmail.com>
2014-08-01 17:27:26 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-machinist <https://github.com/doowb/assemble-machinist>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
